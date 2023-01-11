76a3fcfeebb32c6423ddbd526d720ac925e86d5b
Ubuntu <ubuntu@ip-172-31-35-228.ap-south-1.compute.internal>
2023-01-11 20:05:17 +0000

    dockerfile added

--- config.py
+++ config.py
@@ -5,5 +5,6 @@ mysql = MySQL()
 app.config['MYSQL_DATABASE_USER'] = 'sathvik'
 app.config['MYSQL_DATABASE_PASSWORD'] = 'S@thvikkk2022'
 app.config['MYSQL_DATABASE_DB'] = 'shuttlebus'
-app.config['MYSQL_DATABASE_HOST'] = '3.110.216.13'
+app.config['MYSQL_DATABASE_HOST'] = '3.110.187.184'
+#public ip of 2nd instance.
 mysql.init_app(app)
