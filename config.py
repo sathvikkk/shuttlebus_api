63bbd8c01756e3a13b249c081ea08d275d803c95
Sathvik k k <50857156+sathvikkk@users.noreply.github.com>
2023-01-30 12:57:57 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,6 +5,6 @@ mysql = MySQL()
 app.config['MYSQL_DATABASE_USER'] = 'sathvik'
 app.config['MYSQL_DATABASE_PASSWORD'] = 'S@thvikkk2022'
 app.config['MYSQL_DATABASE_DB'] = 'shuttlebus'
-app.config['MYSQL_DATABASE_HOST'] = '15.206.153.147'
+app.config['MYSQL_DATABASE_HOST'] = '13.232.170.175'
 #public ip of 2nd instance.
 mysql.init_app(app)
