499d512da60409c4e9a19a90e563f2dac8f944fc
Sathvik k k <50857156+sathvikkk@users.noreply.github.com>
2023-01-30 10:16:37 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,9 +2,9 @@ from app import app
 from flaskext.mysql import MySQL
 
 mysql = MySQL()
-app.config['MYSQL_DATABASE_USER'] = 'root'
+app.config['MYSQL_DATABASE_USER'] = 'sathvik'
 app.config['MYSQL_DATABASE_PASSWORD'] = 'S@thvikkk2022'
 app.config['MYSQL_DATABASE_DB'] = 'shuttlebus'
-app.config['MYSQL_DATABASE_HOST'] = '127.0.0.1'
+app.config['MYSQL_DATABASE_HOST'] = '15.206.153.147'
 #public ip of 2nd instance.
 mysql.init_app(app)
